f95d59a6bac6f8d2ec95ba81fb6d1174f779d9c3
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 19:55:35 +0000

    add excepts

--- doc/examples/plot_construct_is2class.py
+++ doc/examples/plot_construct_is2class.py
@@ -14,14 +14,16 @@ print(sys.path)
 
 try:
     import icepyx
-
+except: 
+    pass
 
 sys.path.append(os.path.abspath('../../icepyx/core'))
 print(sys.path)
 
 try:
     import icepyx
-    
+except: 
+    pass    
 
 ipdf = icepyx.is2class.Icesat2Data('ATL06',[-55, 68, -48, 71],['2019-02-20','2019-02-20'], \
                            start_time='00:00:00', end_time='23:59:59', version='2')
