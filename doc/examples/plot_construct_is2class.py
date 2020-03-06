8b514e4b453f96575f242dbb957dff2b59cd7a69
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 19:27:32 +0000

    fix path to find libraries

--- doc/examples/plot_construct_is2class.py
+++ doc/examples/plot_construct_is2class.py
@@ -6,8 +6,9 @@ Construct is2class
 This example shows how to instantiate an is2class object
 """
 
+import os
 import sys
-sys.path.append(sys.path+'/icepyx/core/')
+sys.path.append(os.path.abspath('../../icepyx/core/'))
 
 #
 import is2class as ipd
