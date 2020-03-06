55fc0cd9a2ac5e7ee117863a2b49c03ec3aec526
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 16:30:38 +0000

    add print statement to debug

--- doc/examples/plot_construct_is2class.py
+++ doc/examples/plot_construct_is2class.py
@@ -6,7 +6,9 @@ Construct is2class
 This example shows how to instantiate an is2class object
 """
 
+print("starting at the beginning")
 import sys
+print("sys was imported")
 sys.path.append('../../icepyx/core/')
 print(sys.path)
 
