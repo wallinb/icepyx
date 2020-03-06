b7abd555214a9bde354d0486e98fa7944f5a2f18
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 19:51:44 +0000

    add test

--- doc/examples/plot_construct_is2class.py
+++ doc/examples/plot_construct_is2class.py
@@ -21,3 +21,10 @@ print(sys.path)
 
 try:
     import icepyx
+    
+
+ipdf = icepyx.is2class.Icesat2Data('ATL06',[-55, 68, -48, 71],['2019-02-20','2019-02-20'], \
+                           start_time='00:00:00', end_time='23:59:59', version='2')
+
+
+print(ipdf)
