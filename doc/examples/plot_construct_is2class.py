a4442a549d5c5c669fb92fcaff8051f5db1da43a
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 16:48:08 +0000

    modify header of docstring

---
+++ doc/examples/plot_construct_is2class.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Construct is2class
+==================
+
+This example shows how to instantiate an is2class object
+"""
+
+print("starting at the beginning")
+import sys
+print("sys was imported")
+sys.path.append('../../icepyx/core/')
+print(sys.path)
