813a4774f6ac74786475d3e703f28ccee7305c7c
JessicaS11 <jessica.scheick@maine.edu>
2020-03-06 17:05:20 +0000

    revert to icepyx gallery example

--- doc/examples/plot_construct_is2class.py
+++ doc/examples/plot_construct_is2class.py
@@ -6,8 +6,15 @@ Construct is2class
 This example shows how to instantiate an is2class object
 """
 
-print("starting at the beginning")
 import sys
-print("sys was imported")
-sys.path.append('../../icepyx/core/')
-print(sys.path)
+sys.path.append(sys.path+'/icepyx/core/')
+
+#
+import is2class as ipd
+#
+
+ipdf = ipd.Icesat2Data('ATL06',[-55, 68, -48, 71],['2019-02-20','2019-02-20'], \
+                           start_time='00:00:00', end_time='23:59:59', version='2')
+
+
+print(ipdf)
